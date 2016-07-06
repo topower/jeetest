a0791491537278b50793ec075b674cfbee1c57d9
tomaszp <tomaszp@kainos.com>
2016-07-06 19:13:54 +0100

    Switch to maven

--- sbt
+++
@@ -1,3 +0,0 @@
-SBT_OPTS="-Xms64M -Xmx384M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
-
---
+++ src/main/java/com/airhacks/TomekTestRest.java
@@ -0,0 +1,14 @@
+package com.airhacks;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/xxx")
+public class TomekTestRest {
+
+    @GET
+    public String xxx() {
+        return "hejjjkkkkkka";
+    }
+
+}
